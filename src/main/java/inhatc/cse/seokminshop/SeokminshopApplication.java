cde5052510ce9499bf6cdcc022ddc56cbe3cf892
Tommy Hong <201745035@itc.ac.kr>
2024-09-04 13:43:47 +0900

    Spring Framework Week 2 Study
    Recreating SpringBoot Framework project via web.
    
    Dependency :
    
    Spring Dev Tools
    Lombok
    Spring Web Service
    Thymeleaf
    
    Spring JPA
    MySQL

---
+++ src/main/java/inhatc/cse/seokminshop/SeokminshopApplication.java
@@ -0,0 +1,13 @@
+package inhatc.cse.seokminshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeokminshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeokminshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/inhatc/cse/seokminshop/SeokminshopApplicationTests.java
@@ -0,0 +1,13 @@
+package inhatc.cse.seokminshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeokminshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
