3c270a568547d434b45c01b005d926b5d3aae280
Kruzgesat <tommy9333@naver.com>
2024-10-15 23:19:05 +0900

    Add files via upload
    SpringBoot Framework Week 7
    
    Querydsl Testing
    
    Build the Qitem with gradle to prepare database entities
    
    Using JPAQueryFactory to Transact SQL Queries via JAVA
    
    Paging to get contents.

--- seokminshop/src/test/java/inhatc/cse/seokminshop/item/repository/ItemRepositoryTest.java
+++ seokminshop/src/test/java/inhatc/cse/seokminshop/item/repository/ItemRepositoryTest.java
@@ -1,25 +1,57 @@
 package inhatc.cse.seokminshop.item.repository;
 
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.Tuple;
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import inhatc.cse.seokminshop.item.constant.ItemSellStatus;
 import inhatc.cse.seokminshop.item.entity.Item;
+import inhatc.cse.seokminshop.item.entity.QItem;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.util.Assert;
+import org.thymeleaf.util.StringUtils;
 
 import java.util.List;
 
+import static inhatc.cse.seokminshop.item.entity.QItem.item;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+//@Transactional
 class ItemRepositoryTest {
 
     @Autowired
     private ItemRepository itemRepository;
 
+    @PersistenceContext
+    private EntityManager em;
+
     @Test
     public void test() {
         System.out.println("testsetssetetst");
+    }
+
+    public void createItemList(){
+        for(int i=1; i<=10; i++){
+            Item item = Item.builder()
+                    .itemNm("테스트 상품 " + i)
+                    .price(10000 + i)
+                    .stockNumber(100 + i)
+                    .itemDetail("테스트 상품 상세 설명 " + i)
+                    .itemSellStatus(ItemSellStatus.SELL)
+                    .build();
+
+            itemRepository.save(item);
+        }
     }
 
     @Test
@@ -49,6 +81,80 @@ class ItemRepositoryTest {
         itemList.forEach(System.out::println);
     }
 
+    public void createItemList2(){
+        for(int i=1; i<=5; i++){
+            Item item = Item.builder()
+                    .itemNm("테스트 상품 " + i)
+                    .price(10000 + i)
+                    .stockNumber(100 + i)
+                    .itemDetail("테스트 상품 상세 설명 " + i)
+                    .itemSellStatus(ItemSellStatus.SELL)
+                    .build();
+
+            itemRepository.save(item);
+        }
+
+        for(int i=6; i<=10; i++){
+            Item item = Item.builder()
+                    .itemNm("테스트 상품 " + i)
+                    .price(10000 + i)
+                    .stockNumber(100 + i)
+                    .itemDetail("테스트 상품 상세 설명 " + i)
+                    .itemSellStatus(ItemSellStatus.SOLD_OUT)
+                    .build();
+
+            itemRepository.save(item);
+        }
+    }
+
+    @Test
+    @DisplayName("Querydsl 테스트 2")
+    public void querydslTest2() {
+        createItemList2();
+
+        BooleanBuilder builder = new BooleanBuilder();
+        String itemDetail = "테스트";
+        int price = 10004;
+        String itemSellStatus = "SELL";
+
+
+
+        QItem item = QItem.item;
+
+        builder.and(item.itemDetail.like("%" + itemDetail + "%"));
+        builder.and(item.price.gt(price));
+
+        if(StringUtils.equals(itemSellStatus, ItemSellStatus.SELL)) {
+            builder.and(item.itemSellStatus.eq(ItemSellStatus.SELL));
+        }
+
+        Pageable pageable = PageRequest.of(0, 5);
+
+        Page<Item> page = itemRepository.findAll(builder, pageable);
+        List<Item> content = page.getContent();
+        content.stream().forEach((e) -> {
+            System.out.println(e);
+        });
+
+    }
+
+    //@BeforeEach
+    @Test
+    @DisplayName("Querydsl 테스트")
+    public void querydslTest() {
+        createItemList();
+        JPAQueryFactory query = new JPAQueryFactory(em);
+        QItem qItem = item;
+
+        List<Item> itemList = query.selectFrom(item)
+                            .where(item.itemSellStatus.eq(ItemSellStatus.SELL))
+                            .where(item.itemDetail.like("%"+"1"+"%"))
+                            .orderBy(item.price.desc())
+                            .fetch();
+
+
+        itemList.forEach(item -> System.out.println(item));
+    }
 
 
 }
